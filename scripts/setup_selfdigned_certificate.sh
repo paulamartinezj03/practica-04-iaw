8e7735bc2f2a016ebf7cd143e0bb40ff1fbf6707
Paula <pmarjim089@g.educaand.es>
2025-10-17 08:44:55 +0000

    cambios

--- scripts/setup_selfdigned_certificate.sh
+++ scripts/setup_selfdigned_certificate.sh
@@ -2,3 +2,12 @@
 set -ex
 #Importamos el archivo .env
 source .env
+# Creamos certificado autofirmado
+sudo openssl req \
+  -x509 \
+  -nodes \
+  -days 365 \
+  -newkey rsa:2048 \
+  -keyout /etc/ssl/private/apache-selfsigned.key \
+  -out /etc/ssl/certs/apache-selfsigned.crt \
+  -subj "/C=$OPENSSL_COUNTRY/ST=$OPENSSL_PROVINCE/L=$OPENSSL_LOCALITY/O=$OPENSSL_ORGANIZATION/OU=$OPENSSL_ORGUNIT/CN=$OPENSSL_COMMON_NAME/emailAddress=$OPENSSL_EMAIL"
