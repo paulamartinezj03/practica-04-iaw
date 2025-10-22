ead4f599e0688f5116b5bfb19216325d34985334
Paula <pmarjim089@g.educaand.es>
2025-10-22 10:58:57 +0000

    cambios

--- scripts/setup_selfdigned_certificate.sh
+++ scripts/setup_selfdigned_certificate.sh
@@ -11,7 +11,18 @@ sudo openssl req \
   -keyout /etc/ssl/private/apache-selfsigned.key \
   -out /etc/ssl/certs/apache-selfsigned.crt \
   -subj "/C=$OPENSSL_COUNTRY/ST=$OPENSSL_PROVINCE/L=$OPENSSL_LOCALITY/O=$OPENSSL_ORGANIZATION/OU=$OPENSSL_ORGUNIT/CN=$OPENSSL_COMMON_NAME/emailAddress=$OPENSSL_EMAIL"
-# Copiamos el archivo default-sl.conf al servidor
+#Copiamos el archivo default-ssl.conf al servidor
 cp ../conf/default-ssl.conf /etc/apache2/sites-available
-#Habilitamos el modulo ssl 
-sudo a2ensite default-ssl.conf
+#Habilitamos el sitio default-ssl.conf
+a2ensite default-ssl.conf
+
+#Habilitamos el modulo ssl de apache
+sudo a2enmod ssl
+#Copiamos el archivo de configuración de apache para redirigir HTTP a HTTPS
+cp ../conf/000-default.conf /etc/apache2/sites-available
+
+#Habilitamos el modulo rewrite de apache
+sudo a2enmod rewrite
+
+#Reiniciamos apache para aplicar los cambios
+systemctl restart apache2
